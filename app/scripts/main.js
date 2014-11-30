ed4ab34dd0394ac5555070ebc068dd712e551a73
Benji Shine <ben@openben.net>
2014-11-29 17:25:02 -0800

    Initial version. Has bootstrap, backbone.

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Sunrise, $*/
+
+
+window.Sunrise = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Sunrise.init();
+});
