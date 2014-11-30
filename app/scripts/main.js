af189003761acede67c4e9707edefe49d598ef2f
Benji Shine <ben@openben.net>
2014-11-29 17:42:45 -0800

    Rendering a single colorstop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,16 @@ window.Sunrise = {
     init: function () {
         'use strict';
         console.log('Hello from Backbone!');
+        var thing = new this.Models.Colorstop();
+        window.thing = thing;
+
+        var collection = new this.Collections.Colorstops();
+        window.collection = collection;
+
+        collection.add(thing);
+
+        var view = new this.Views.Colorstop( { model: thing, el: $('.colorstop-1') });
+        view.render();
     }
 };
 
