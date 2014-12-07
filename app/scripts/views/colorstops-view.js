73c8cb9e130a19fb3f2b80eef8c7933cf96b6c02
Benji Shine <ben@openben.net>
2014-12-06 20:06:17 -0800

    Moved animated preview logic into SunriseAnimatedView class

--- app/scripts/views/colorstops-view.js
+++ app/scripts/views/colorstops-view.js
@@ -76,32 +76,8 @@ Sunrise.Views.Colorstops = Backbone.View.extend({
 
     context.fillStyle = gradient;
     context.fillRect(0, 0, 600, 300);
-  },
-
-  // TODO: make a new backbone view for this. Share the same collection.
-  animatePreview: function () {
-    var duration = 4000; // milliseconds
-    if (this.collection.length === 0) {
-      console.log("nothing to animate");
-      return;
-    }
-    var stepDuration = duration / this.collection.length;
-    var $elemToAnimate = $('.modal-preview');
-    $elemToAnimate.css('background-color', this.collection.at(0).get('color'));
-    $elemToAnimate.show();
-
-    this.collection.forEach(function (el, index) {
-      var color = el.get('color');
-      window.setTimeout(function() {
-        $elemToAnimate.animate( { backgroundColor: color }, stepDuration);
-      }, index * stepDuration);
-    });
-
-    window.setTimeout(function () {
-      $elemToAnimate.hide();
-    }, duration + 3000)
-
   }
+
 });
 
 
--- app/scripts/views/sunrise-animated-view.js
+++ app/scripts/views/sunrise-animated-view.js
@@ -19,12 +19,37 @@ Sunrise.Views = Sunrise.Views || {};
 
     render: function () {
       this.$el.html(this.template());
+      this.$preview = $('.modal-preview');
     },
 
     animatePreview: function () {
       this.$('.modal-preview').show();
+      var duration = Sunrise.Views.SunriseAnimatedView.PREVIEW_DURATION; // milliseconds
+      if (this.collection.length === 0) {
+        console.log("nothing to animate");
+        return;
+      }
+
+      var stepDuration = duration / this.collection.length;
+      var $preview = this.$preview;
+      $preview.css('background-color', this.collection.at(0).get('color'));
+      $preview.show();
+
+      this.collection.forEach(function (el, index) {
+        var color = el.get('color');
+        window.setTimeout(function() {
+          $preview.animate( { backgroundColor: color }, stepDuration);
+        }, index * stepDuration);
+      });
+
+      window.setTimeout(function () {
+        $preview.hide();
+      }, duration + 3000)
+      // TODO: add replay & dismiss buttons
     }
 
+  }, {
+    PREVIEW_DURATION: 1000
   });
 
 })();
