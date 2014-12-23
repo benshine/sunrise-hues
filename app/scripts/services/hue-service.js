566500a935c91d25f196b63b3b51552d6eb3a06e
Benji Shine <ben@openben.net>
2014-12-23 15:29:45 -0800

    Use an IIFE to create the HueService exactly once, but hide its privates in a closure.

--- app/scripts/services/hue-service.js
+++ app/scripts/services/hue-service.js
@@ -1,48 +1,47 @@
-var HueService = {
+var HueService = HueService || function () {
+  var username = 'newdeveloper';
+  var bridgeAddress = 'http://10.0.1.3';
+  var lights = [1, 2, 3];
 
-  // Hardcoded values for my home setup.
-  // TODO: make configurable by user
-  username: 'newdeveloper',
-  bridgeAddress: 'http://10.0.1.3',
-  lights: [1, 2, 3],
+  return {
+    allOn: function (color) {
+      lights.forEach(function (val) {
+        this.toggleLight(val, true, color);
+      }, this);
+    },
 
-  allOn: function (color) {
-    this.lights.forEach(function (val) {
-      this.toggleLight(val, true, color);
-    }, this);
-  },
+    toggleLight: function (light, on, color) {
+      var data = { "on" : on };
+      if (color) {
+        Object.assign(data, this.colorToHueHsv(color));
 
-  toggleLight: function (light, on, color) {
-    var data = { "on" : on };
-    if (color) {
-      Object.assign(data, this.colorToHueHsv(color));
-
-    }
-    this.put(this.buildUrl('lights/' + light + '/state'), JSON.stringify(data));
-  },
-
-  put: function (url, data) {
-    return $.ajax(
-      url,
-      {
-        type: 'PUT',
-        data: data,
-        processData: false
       }
-    );
-  },
+      this.put(this.buildUrl('lights/' + light + '/state'), JSON.stringify(data));
+    },
 
-  buildUrl: function (path) {
-    return this.bridgeAddress + '/api/' + this.username + '/' + path;
-  },
+    put: function (url, data) {
+      return $.ajax(
+        url,
+        {
+          type: 'PUT',
+          data: data,
+          processData: false
+        }
+      );
+    },
 
-  colorToHueHsv: function (color) {
-    var jqc = $.Color(color);
+    buildUrl: function (path) {
+      return bridgeAddress + '/api/' + username + '/' + path;
+    },
 
-    return {
-      "hue" : Math.floor(65535 * jqc.hue() / 360),
-      "sat": Math.floor(jqc.saturation() * 255),
-      "bri": Math.floor(jqc.lightness() * 255)
+    colorToHueHsv: function (color) {
+      var jqc = $.Color(color);
+
+      return {
+        "hue" : Math.floor(65535 * jqc.hue() / 360),
+        "sat": Math.floor(jqc.saturation() * 255),
+        "bri": Math.floor(jqc.lightness() * 255)
+      }
     }
   }
-};
+}();
