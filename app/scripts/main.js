204016cd967943d1d436e802a442427343d2d95b
Benji Shine <ben@openben.net>
2014-12-24 13:54:16 -0800

    Requirejs: Index loads with no errors, just loading backbone and underscore. No app logic loaded.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,30 @@
 /*global Sunrise, $*/
 
+requirejs.config({
+  paths: {
+    bower: '../bower_components',
+    jquery: '../bower_components/jquery/dist/jquery',
+    backbone: '../bower_components/backbone',
+    underscore: '../bower_components/underscore'
+  }
+});
+
+
+requirejs([
+    'jquery',
+    'bower/underscore/underscore',
+    'bower/backbone/backbone'
+  ],
+  function   ($, Underscore, Backbone) {
+    //jQuery, canvas and the app/sub module are all
+    //loaded and can be used here now.
+    console.log("yes, we are here!");
+    $('body').css('background-color', '#F00');
+    console.log("Do we have _? ", _);
+    console.log("Do we have backbone? ", Backbone.View);
+  });
+
+/*
 
 window.Sunrise = {
   Models: {},
@@ -37,3 +62,4 @@ $(document).ready(function () {
   'use strict';
   Sunrise.init();
 });
+*/
