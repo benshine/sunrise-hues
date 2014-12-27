b18be389b96433f12a82eb3476c1853e2dc8bc33
Benji Shine <ben@openben.net>
2014-12-26 19:38:51 -0800

    Whoops, actually use jquery-color to turn colors to HSV -- make the colors actually get installed on the bulbs!

--- app/scripts/services/hue-service.js
+++ app/scripts/services/hue-service.js
@@ -1,4 +1,3 @@
-// TODO: this needs jquery-color to really work
 define(['jquery', 'bower/es6-shim/es6-shim'], function ($) {
 
   return function () {
@@ -16,8 +15,7 @@ define(['jquery', 'bower/es6-shim/es6-shim'], function ($) {
       toggleLight: function (light, on, color) {
         var data = {"on": on};
         if (color) {
-          var asHsv = this.colorToHueHsv(color);
-          Object.assign(data, asHsv);
+          Object.assign(data, this.colorToHueHsv(color));
         }
         this.put(this.buildUrl('lights/' + light + '/state'), JSON.stringify(data));
       },
@@ -38,15 +36,7 @@ define(['jquery', 'bower/es6-shim/es6-shim'], function ($) {
       },
 
       colorToHueHsv: function (color) {
-        // TODO: make jquery-color work in requirejs!
-        // var jqc = $.Color(color);
-        // Until I fix jquery-color in requirejs, just pretend
-        // all colors are something hardcoded
-        var jqc = {
-          hue: function () { return 0.5; },
-          saturation: function () { return 0.5; },
-          lightness: function () { return 0.5; }
-        };
+        var jqc = $.Color(color);
 
         return {
           "hue": Math.floor(65535 * jqc.hue() / 360),
