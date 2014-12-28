db95c9e83ce0d04b1d910fefadbf16ada7a77fc3
Benji Shine <ben@openben.net>
2014-12-28 15:48:53 -0800

    Added karma and jasmine, via karma init. Now I have both jasmine and mocha, which is not so great, and my tests aren't really testing anything.  To run the nascent tests: `karma start karma.conf.js`

--- test/spec/test.js
+++ test/spec/test.js
@@ -6,7 +6,7 @@
   describe('Give it some context', function () {
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
-
+        expect(false).toBeTruthy();
       });
     });
   });
